de251038df4c1658ed63dd7fae1f6d9bfb818f3a
tuska <tuskafalls@gmail.com>
2022-07-22 04:06:21 +0430

    math operators first commit for learning python operators

---
+++ math operators.py
@@ -0,0 +1,26 @@
+x = 7
+y = 4.1
+z = 0
+print(x - 1)
+# Algebra
+c = x + y - 2
+print(c)
+#  Algebra ends
+# multiply python: 28.69 but it should be : 28.7 THINK ABOUT IT
+print(x * y)
+# divide
+print(4 / 3)
+print(3 / 4)
+# power (Tavan)
+print(3 ** 2)
+# square root (Jazr)
+print(9 ** (1/2))
+print(81 ** (1/2))
+
+# the + and * operator can be used with strings
+firstname = 'Daniel'
+lastname = 'Ratclif'
+print(firstname + ' ' + lastname)
+print(firstname + lastname)
+print(firstname * 5)
+
